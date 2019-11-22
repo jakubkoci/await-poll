149b0b64dd45ddce96e6450a7b0088f04cdc4e89
jakubkoci <jakub.koci@gmail.com>
2019-11-22 15:01:57 +0100

    Fix async polyfills setup

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,10 @@ module.exports = {
       {
         targets: {
           node: 'current',
+          edge: '17',
+          firefox: '60',
+          chrome: '67',
+          safari: '11.1',
         },
       },
     ],
